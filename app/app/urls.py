6f1c8cdaf67303d5e036acde378771f63c8bbba4
Felipe Borges <felipejsborges@outlook.com>
2024-05-03 18:54:02 -0300

    setup for deploy

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from core import views as core_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,6 +24,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/health-check/", core_views.health_check, name="health-check"),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
         "api/docs/",
