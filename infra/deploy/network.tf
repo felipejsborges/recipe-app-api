6f057832b9ae302a22aae4229ea9beb22431c671
Felipe Borges <felipejsborges@outlook.com>
2024-06-04 19:15:21 -0300

    add route table

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -14,26 +14,28 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}"
+  }
+}
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
 # resource "aws_subnet" "public_a" {
 #   vpc_id                  = aws_vpc.main.id
 #   cidr_block              = "10.1.1.0/24"
 #   map_public_ip_on_launch = true
 #   availability_zone       = "${data.aws_region.current.name}a"
 # }
-
-# resource "aws_route_table" "public_a" {
-#   vpc_id = aws_vpc.main.id
-# }
-
 # resource "aws_route_table_association" "public_a" {
 #   subnet_id      = aws_subnet.public_a.id
-#   route_table_id = aws_route_table.public_a.id
-# }
-
-# resource "aws_route" "public_internet_access_a" {
-#   route_table_id         = aws_route_table.public_a.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.main.id
+#   route_table_id = aws_route_table.main.id
 # }
 
 # resource "aws_subnet" "public_b" {
@@ -42,21 +44,11 @@ resource "aws_internet_gateway" "main" {
 #   map_public_ip_on_launch = true
 #   availability_zone       = "${data.aws_region.current.name}b"
 # }
-
-# resource "aws_route_table" "public_b" {
-#   vpc_id = aws_vpc.main.id
-# }
-
 # resource "aws_route_table_association" "public_b" {
 #   subnet_id      = aws_subnet.public_b.id
-#   route_table_id = aws_route_table.public_b.id
+#   route_table_id = aws_route_table.main.id
 # }
 
-# resource "aws_route" "public_internet_access_b" {
-#   route_table_id         = aws_route_table.public_b.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.main.id
-# }
 
 # resource "aws_subnet" "private_a" {
 #   vpc_id            = aws_vpc.main.id
