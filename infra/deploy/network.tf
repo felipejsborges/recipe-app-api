f03c711221ba2427008c2f040e713f816f5c1c5f
Felipe Borges <felipejsborges@outlook.com>
2024-06-04 19:17:09 -0300

    add subnets

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -27,40 +27,40 @@ resource "aws_route" "public_internet_access" {
   gateway_id             = aws_internet_gateway.main.id
 }
 
-# resource "aws_subnet" "public_a" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.1.1.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "${data.aws_region.current.name}a"
-# }
-# resource "aws_route_table_association" "public_a" {
-#   subnet_id      = aws_subnet.public_a.id
-#   route_table_id = aws_route_table.main.id
-# }
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a"
+}
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.main.id
+}
 
-# resource "aws_subnet" "public_b" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.1.2.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "${data.aws_region.current.name}b"
-# }
-# resource "aws_route_table_association" "public_b" {
-#   subnet_id      = aws_subnet.public_b.id
-#   route_table_id = aws_route_table.main.id
-# }
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b"
+}
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.main.id
+}
 
 
-# resource "aws_subnet" "private_a" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.1.10.0/24"
-#   availability_zone = "${data.aws_region.current.name}a"
-# }
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+}
 
-# resource "aws_subnet" "private_b" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.1.11.0/24"
-#   availability_zone = "${data.aws_region.current.name}b"
-# }
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+}
 
 # resource "aws_security_group" "endpoint_access" {
 #   description = "Access to endpoints"
