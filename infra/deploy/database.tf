fc169d5a8e642e02f9b363cb93c096da4a4a2ee7
Felipe Borges <felipejsborges@outlook.com>
2024-06-07 18:52:36 -0300

    add rds

---
+++ infra/deploy/database.tf
@@ -0,0 +1,42 @@
+############
+# Database #
+############
+
+resource "aws_db_subnet_group" "main" {
+  name = "${local.prefix}-main"
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+}
+
+resource "aws_security_group" "rds" {
+  description = "Allow access to the RDS database instance."
+  name        = "${local.prefix}-ds"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id            = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.rds.id
+  ip_protocol                  = "tcp"
+  from_port                    = 5432
+  to_port                      = 5432
+}
+
+resource "aws_db_instance" "main" {
+  identifier        = local.prefix
+  db_name           = "my_db"
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "postgres"
+  # engine_version             = "15.3"
+  auto_minor_version_upgrade = false
+  instance_class             = "db.t3.micro"
+  username                   = var.db_username
+  password                   = var.db_password
+  skip_final_snapshot        = true # for real projects should be false
+  db_subnet_group_name       = aws_db_subnet_group.main.name
+  multi_az                   = false
+  vpc_security_group_ids     = [aws_security_group.rds.id]
+}
