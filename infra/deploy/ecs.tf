90a92883659fd9294f50e027edeb904ffe69311e
Felipe Borges <felipejsborges@outlook.com>
2024-06-17 19:17:15 -0300

    add roles

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -2,39 +2,39 @@
 # ECS Cluster for running app on Fargate.
 ##
 
-# resource "aws_iam_policy" "task_execution_role_policy" {
-#   name        = "${local.prefix}-task-exec-role-policy"
-#   path        = "/"
-#   description = "Allow ECS to retrieve images and add to logs."
-#   policy      = file("./templates/ecs/task-execution-role-policy.json")
-# }
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = file("./templates/ecs/task-execution-role-policy.json")
+}
 
-# resource "aws_iam_role" "task_execution_role" {
-#   name               = "${local.prefix}-task-execution-role"
-#   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
-# }
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
 
-# resource "aws_iam_role_policy_attachment" "task_execution_role" {
-#   role       = aws_iam_role.task_execution_role.name
-#   policy_arn = aws_iam_policy.task_execution_role_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
 
-# resource "aws_iam_role" "app_task" {
-#   name               = "${local.prefix}-app-task"
-#   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
-# }
+resource "aws_iam_role" "app_task" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
+}
 
-# resource "aws_iam_policy" "task_ssm_policy" {
-#   name        = "${local.prefix}-task-ssm-role-policy"
-#   path        = "/"
-#   description = "Policy to allow System Manager to execute in container"
-#   policy      = file("./templates/ecs/task-ssm-policy.json")
-# }
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "Policy to allow System Manager to execute in container"
+  policy      = file("./templates/ecs/task-ssm-policy.json")
+}
 
-# resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
-#   role       = aws_iam_role.app_task.name
-#   policy_arn = aws_iam_policy.task_ssm_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
+  role       = aws_iam_role.app_task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
 
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   name = "${local.prefix}-api"
@@ -50,8 +50,8 @@ resource "aws_ecs_task_definition" "api" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  # execution_role_arn       = aws_iam_role.task_execution_role.arn # ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume.
-  # task_role_arn            = aws_iam_role.app_task.arn # ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services.
+  execution_role_arn       = aws_iam_role.task_execution_role.arn # ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume.
+  task_role_arn            = aws_iam_role.app_task.arn            # ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services.
 
   volume {
     name = "static"
